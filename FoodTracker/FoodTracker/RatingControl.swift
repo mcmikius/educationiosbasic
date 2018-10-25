ceda68acdd13eb63affad97f62503587890327d9
Michail Bondarenko <mcmikius15@gmail.com>
2018-10-25 09:47:49 +0300

    Add Support for Interface Builder

--- FoodTracker/FoodTracker/RatingControl.swift
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -8,9 +8,25 @@
 
 import UIKit
 
-class RatingControl: UIStackView {
+
+
+//class RatingControl: UIStackView {
+    
+@IBDesignable class RatingControl: UIStackView {
     
     //MIKE: Properties
+    
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
+        didSet {
+            setupButton()
+        }
+    }
+    @IBInspectable var starCount: Int = 5 {
+        didSet{
+            setupButton()
+        }
+    }
+    
     private var ratingButtons = [UIButton]()
     
     var rating = 0
@@ -37,15 +53,22 @@ class RatingControl: UIStackView {
     
     private func setupButtons() {
         
-        for _ in 0..<5 {
+        // clear any existing buttons
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+        
+        for _ in 0..<starCount {
         // Create the button
         let button = UIButton()
         button.backgroundColor = UIColor.red
         
         // Add constraints
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
         
         // Setup the button action
         button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
@@ -58,6 +81,8 @@ class RatingControl: UIStackView {
             
         }
         
+        }
+        
     }
 
-}
+//}
