f6c162bb2805363b3a3d19bc5499991f7a041890
Michail Bondarenko <mcmikius15@gmail.com>
2018-11-07 16:12:27 +0200

    add view controller email

--- LogInSignUp/LogInSignUp/ViewController.swift
+++ LogInSignUp/LogInSignUp/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         registration.layer.shadowOpacity = 1
-        registration.layer.shadowColor = UIColor.red.cgColor
+        registration.layer.shadowColor = UIColor.black.cgColor
     }
     @IBAction func registration(_ sender: UIButton) {
     }
