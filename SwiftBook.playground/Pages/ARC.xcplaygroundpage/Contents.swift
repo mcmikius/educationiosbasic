67c32d002a9b63fb64f27da2277aaefb8a91c604
Michail Bondarenko <mcmikius15@gmail.com>
2018-12-11 09:44:38 +0200

    ARC part one

---
+++ SwiftBook.playground/Pages/ARC.xcplaygroundpage/Contents.swift
@@ -0,0 +1,151 @@
+/*
+ class Person {
+    let name: String
+    init(name: String) {
+        self.name = name
+        print("\(name) инициализируется")
+    }
+    deinit {
+        print("\(name) деинициализируется")
+    }
+}
+
+var reference1: Person?
+var reference2: Person?
+var reference3: Person?
+reference1 = Person(name: "John Appleseed")
+reference2 = reference1
+reference3 = reference1
+
+reference1 = nil
+reference2 = nil
+reference3 = nil
+ */
+/*
+class Person {
+    let name: String
+    init(name: String) { self.name = name }
+    var apartment: Apartment?
+    deinit { print("\(name) освобождается") }
+}
+  
+class Apartment {
+    let unit: String
+    init(unit: String) { self.unit = unit }
+    var tenant: Person?
+    deinit { print("Апартаменты \(unit) освобождаются") }
+}
+var john: Person?
+var unit4A: Apartment?
+
+john = Person(name: "John Appleseed")
+unit4A = Apartment(unit: "4A")
+
+john!.apartment = unit4A
+unit4A!.tenant = john
+
+john = nil
+unit4A = nil
+*/
+/*
+class Person {
+    let name: String
+    init(name: String) { self.name = name }
+    var apartment: Apartment?
+    deinit { print("\(name) деинициализируется") }
+}
+  
+class Apartment {
+    let unit: String
+    init(unit: String) { self.unit = unit }
+    weak var tenant: Person?
+    deinit { print("Apartment \(unit) деинициализируется") }
+}
+var john: Person?
+var unit4A: Apartment?
+  
+john = Person(name: "John Appleseed")
+unit4A = Apartment(unit: "4A")
+  
+john!.apartment = unit4A
+unit4A!.tenant = john
+
+john = nil
+unit4A = nil
+*/
+class Customer {
+    let name: String
+    var card: CreditCard?
+    init(name: String) {
+        self.name = name
+    }
+    deinit { print("\(name) деинициализируется") }
+}
+  
+class CreditCard {
+    let number: UInt64
+    unowned let customer: Customer
+    init(number: UInt64, customer: Customer) {
+        self.number = number
+        self.customer = customer
+    }
+    deinit { print("Карта #\(number) деинициализируется") }
+}
+
+var john: Customer?
+
+john = Customer(name: "John Appleseed")
+john!.card = CreditCard(number: 1234567890123456, customer: john!)
+john = nil
+
+class Country {
+    let name: String
+    var capitalCity: City!
+    init(name: String, capitalName: String) {
+        self.name = name
+        self.capitalCity = City(name: capitalName, country: self)
+    }
+}
+  
+class City {
+    let name: String
+    unowned let country: Country
+    init(name: String, country: Country) {
+        self.name = name
+        self.country = country
+    }
+}
+var country = Country(name: "Россия", capitalName: "Москва")
+print("Столицей страны \(country.name) является \(country.capitalCity.name)")
+
+class HTMLElement {
+    
+    let name: String
+    let text: String?
+    
+    lazy var asHTML: () -> String = {
+        if let text = self.text {
+            return "<\(self.name)>\(text)</\(self.name)>"
+        } else {
+            return "<\(self.name) />"
+        }
+    }
+    
+    init(name: String, text: String? = nil) {
+        self.name = name
+        self.text = text
+    }
+    
+    deinit {
+        print("\(name) деинициализируется")
+    }
+    }
+let heading = HTMLElement(name: "h1")
+let defaultText = "some default text"
+heading.asHTML = {
+    return "<\(heading.name)>\(heading.text ?? defaultText)</\(heading.name)>"
+}
+print(heading.asHTML())
+var paragraph: HTMLElement? = HTMLElement(name: "p", text: "hello, world")
+print(paragraph!.asHTML())
+paragraph = nil
