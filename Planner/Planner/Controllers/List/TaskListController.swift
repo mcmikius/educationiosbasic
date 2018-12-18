ac0cbd03ca85ea931ea9c8e856a475c81c98b459
Michail Bondarenko <mcmikius15@gmail.com>
2018-12-18 09:58:44 +0200

    TableView Cell

---
+++ Planner/Planner/Controllers/List/TaskListController.swift
@@ -0,0 +1,115 @@
+//
+//  TaskListController.swift
+//  Planner
+//
+//  Created by Michail Bondarenko on 12/18/18.
+//  Copyright © 2018 Michail Bondarenko. All rights reserved.
+//
+
+import UIKit
+
+class TaskListController: UITableViewController {
+    
+    //the temporary array for test data
+    private var taskList:[Task] = [
+        Task(name:"Task 1", category:"Category1"),
+        Task(name:"Task 2", category:"Category2"),
+        Task(name:"Task 3", category:"Category3"),
+        Task(name:"Task 4", category:"Category4"),
+        Task(name:"Task 5", category:"Category5"),
+        Task(name:"Task 6", category:"Category6")
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+    
+    //auto methods of TableView
+
+    //numbers of sections
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    //numbers of rows
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return taskList.count
+    }
+
+    //show data in a row
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "testCell", for: indexPath)
+        
+        cell.textLabel?.text = taskList[indexPath.row].name // from array get value by index and view 'name'
+        cell.detailTextLabel?.text = taskList[indexPath.row].category // from array get value by index and view 'category'
+
+        return cell
+    }
+    /*
+    //name for each section
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Section" + String(section + 1)
+    }
+    
+    
+    //height each section
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+ */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Planner/Planner/Objects/Task.swift
@@ -0,0 +1,22 @@
+//
+//  Task.swift
+//  Planner
+//
+//  Created by Michail Bondarenko on 12/18/18.
+//  Copyright © 2018 Michail Bondarenko. All rights reserved.
+//
+
+import Foundation
+
+
+
+class Task{
+    
+    init(name:String, category:String){
+        self.name = name
+        self.category = category
+    }
+    
+    var name:String = "Name"
+    var category:String = "Category"
+}
